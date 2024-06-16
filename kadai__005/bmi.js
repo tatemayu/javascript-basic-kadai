21002161173c36b2de35605a2935405560428f25
tatemayu <164420579+tatemayu@users.noreply.github.com>
2024-06-17 06:52:04 +0900

    Add files via upload

--- kadai__005/bmi.js
+++ kadai__005/bmi.js
@@ -1,8 +1,8 @@
-// 変数の宣言
-let userName;
+const userWeight =68;
+const userHeight= 1.7;
 
-//  値の代入
-userName = 'BMI';
+//BMI計算
+console.log( userWeight/(userHeight* userHeight));
 
 //  コンソールへの出力
 console.log(68/(1.7*1.7))
