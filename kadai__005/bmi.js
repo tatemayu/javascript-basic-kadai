4c300f9477ca36287a83e3c83a88b42d46d730a3
tatemayu <164420579+tatemayu@users.noreply.github.com>
2024-06-19 17:12:15 +0900

    Add files via upload

--- kadai__005/bmi.js
+++ kadai__005/bmi.js
@@ -1,9 +1,7 @@
 let userWeight =68;
 let userHeight= 1.7;
-let BMI = userWeight / userHeight*userHeight;
+let BMI = userWeight / (userHeight*userHeight);
 
-//BMI計算
-console.log( userWeight/(userHeight* userHeight));
+// コンソールへの出力
+console.log(BMI);
 
-//  コンソールへの出力
-console.log(68/(1.7*1.7))
